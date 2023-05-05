cca658e001ccb08a59fa1b4b234e0bdce29d23f7
yogita03 <patilyogita408@gmail.com>
2023-05-05 11:57:29 +0530

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-
-import './App.css';
-
-import SignupForm from "./components/loginSignUp/signUpForm";
-
+// import SignUpForm from "./components/LoginSignUp/SignUpForm";
+import SignUpForm from './components/loginSignUp/signUpForm'
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Pages/HomePage/Home";
 function App() {
   return (
     <div>
-       <SignupForm></SignupForm>
+      <Routes>
+       <Route path="/" element={<SignUpForm/>}></Route>
+       <Route path="/home/:idToken" element={<Home/>}></Route>
+
+       </Routes>
     </div>
   );
 }
-
 
 export default App;
